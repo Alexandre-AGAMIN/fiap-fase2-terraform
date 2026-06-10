46876bc1ef6950cc4a1bbd39a283dcc8bcaa16e0
Alexandre-AGAMIN <alexandrepm2810@gmail.com>
2026-06-10 01:29:28 -0300

    agora o está usando o terraform cloud git push

--- terraform-aula-8-infra-eks/backend.tf
+++ terraform-aula-8-infra-eks/backend.tf
@@ -1,7 +1,17 @@
+# # terraform {
+# #   backend "s3" {
+# #     bucket = "fiap-agamin-terraform-aula-8"
+# #     key    = "aula08/terraform.tfstate"
+# #     region = "us-east-1"
+# #   }
+# # }
+
 terraform {
-  backend "s3" {
-    bucket = "fiap-agamin-terraform-aula-8"
-    key    = "aula08/terraform.tfstate"
-    region = "us-east-1"
+  cloud {
+    organization = "fiap-15soat-terraform"
+    workspaces {
+        name = "terraform-aula-8-infra-eks"
+    }
   }
 }
+
